45ac07607a83760c1c494c40d52c5341f4a9fc2a
Kidon Chu <kidonchu@gmail.com>
2018-09-10 08:27:27 -0500

    init

---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		email: 'Shanna@melissa.tv'
+	},
+  {
+    id: 3,
+    name: 'Patricia Lebsack',
+    email: 'Julianne@kory.org'
+  },
+  {
+    id: 4,
+    name: 'Skylar Choopy',
+    email: 'choopy@whiteboard.org'
+  },
+	{
+		id: 5,
+		name: 'Patric Long',
+		email: 'leee@abc.org'
+	}
+];
+
+// vim:set et ts=2 sw=2 sts=2:
