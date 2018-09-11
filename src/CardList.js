d11b7ff846e739166f88f76b994f48e587017476
Kidon Chu <kidonchu@gmail.com>
2018-09-10 23:17:20 -0500

    check

--- src/CardList.js
+++ src/CardList.js
@@ -2,23 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-	return (
-		<div>
-			{
-				robots.map((user, i) => {
-					return (<Card 
-						key={robots[i].id} 
-						id={robots[i].id} 
-						name={robots[i].name} 
-						email={robots[i].email}
-						/>
-					);
-				})
-			}
-		</div>
-	);
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (<Card
+                        key={robots[i].id}
+                        id={robots[i].id}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                    />
+                    );
+                })
+            }
+        </div>
+    );
 };
 
 export default CardList;
-
-// vim:set et ts=2 sw=2 sts=2:
